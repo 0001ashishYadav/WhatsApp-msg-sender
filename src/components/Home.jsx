5e9fc2f182c5e86ed8685811ac31540e7759b3fd
000ashishYadav <000ashishyadav2003@gmail.com>
2025-04-03 22:11:05 +0530

    some work done in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,24 +8,31 @@ function HomePage() {
 
   const formHandler = (e) => {
     e.preventDefault();
-    navigate(`/${inputValue}`);
+    navigate(`/${inputValue.toLowerCase()}`);
   };
   return (
-    <div className="mx-auto max-w-2xs h-screen flex items-end pb-5 ">
+    <div className="mx-auto max-w-2xs h-screen flex flex-col justify-center items-center pb-5 relative">
+      <section className="border rounded h-[400px] w-[400px] border-white ">
+        <img
+          src="/assets/durga-maa.png"
+          className=" h-[100%] w-[100%] object-contain"
+        />
+      </section>
+
       <form
         onSubmit={formHandler}
-        className="px-5 py-2 border flex items-center gap-4 rounded bg-gray-500/15"
+        className="px-5 py-2 border flex items-center gap-4 rounded bg-gray-500/15 absolute bottom-5 "
       >
         <input
-          className="outline-none text-white font-semibold"
+          className="outline-none text-white font-semibold bg-gray-500/10 rounded pl-2 py-1 "
           type="text"
           onChange={(e) => setInputValue(e.target.value)}
         />
         <button
           type="submit"
-          disabled={inputValue ? false : true}
+          disabled={inputValue.length > 2 ? false : true}
           className={` rounded px-4 py-1 text-white font-semibold ${
-            inputValue ? "bg-green-600" : "bg-red-600"
+            inputValue.length > 2 ? "bg-green-600" : "bg-red-600"
           }`}
         >
           send
